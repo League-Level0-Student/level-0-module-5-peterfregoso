b922305db9bfaad531d9829ead35484562a5cfdd
peter <league@WTS-IM-17.attlocal.net>
2018-06-04 18:25:23 -0700

    fibs, goofy names ip

--- src/Fibonocci.java
+++ src/Fibonocci.java
@@ -1,4 +1,12 @@
 
 public class Fibonocci {
+public static void main(String[] args) {
+	int fib = 1;
 
+	for (int i=0; i<145; i+= fib) {
+		fib+=i;
+		System.out.println(i);
+		System.out.println(fib);
+	}
+}
 }
--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -10,13 +10,22 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+String name = JOptionPane.showInputDialog("what is your name");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
-
+System.out.println(name.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
-
+System.out.println(name.length());
+int j = name.length();
+for (int i= 0; i <j; i++) {
+	String sub= name.substring(i,i+1 );
+	if(i%2== 1) {
+sub.toUpperCase();		
+	
+	}
+	System.out.println(sub);
+}
 			// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
